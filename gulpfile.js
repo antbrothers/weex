5294d62a29c4c1b1f9657ba5834ec4f36512afb2
Administrator <jianxi_lin@163.com>
2018-03-13 13:20:34 +0800

    发布到服务器

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sftp = require('gulp-sftp');
+
+gulp.task('default', function() {
+    return gulp.src('./dist/**')
+    .pipe(sftp({
+        host: '114.67.143.209',
+        port: 22,
+        user: 'root',
+        pass: 'oeusTGa]1',
+    }))
+})
